7bbeec2325a408dcb66d2c2dd9ca7c779655d637
ikang <1007888882@qq.com>
2022-06-29 12:20:58 +0800

    init function-- prepare CoCoNut patches

--- Prepare_Evaluation.py
+++ Prepare_Evaluation.py
@@ -1,6 +1,7 @@
 import codecs
 import json
 
+from CoCoNut.tokenization.tokenization import get_strings_numbers, token2statement
 from Utils.IOHelper import readF2L, readF2L_ori
 
 
@@ -68,5 +69,33 @@ def Prepare_CodeBERTFT_patches(cand_size,preds_f,ids_f,input_dir,output_f):
     Prepare_patches_fromline(cand_size, preds_f, ids_f, input_dir, output_f)
 
 def Prepare_CoCoNut_patches(cand_size,preds_f,ids_f,input_dir,output_f):
-    pass
+    def Recovery_CoCoNut_one(buggy_str, pred_str):
+        strings, numbers = get_strings_numbers(buggy_str)
+        recovery_tokens = pred_str.split()
+        recovery_str = token2statement(recovery_tokens, numbers, strings)
+        # print(recovery_str)
+        if len(recovery_str) == 0:
+            recovery_str = [pred_str]
+        return recovery_str[0]
+    ids=readF2L(ids_f)
+    preds=readF2L(preds_f)
+    assert len(ids)*cand_size==len(preds)
+    patches_all = {}
+    for idx,id in enumerate(ids):
+        patches_id = {}
+        id_metas = codecs.open(input_dir + "/metas/" + id + '.txt', 'r', encoding='utf8').read().strip()
+        err_line = int(str(id_metas.split("<sep>")[2])[1:-1].split(":")[0])
+        buggy_line = codecs.open(input_dir + '/buggy_lines/' + id + ".txt").read().strip()
+        buggy_method = readF2L_ori(input_dir + "/buggy_methods/" + id + '.txt')
+        assert (buggy_line in buggy_method[err_line])
+        all_candidates = preds[idx * cand_size:(idx + 1) * cand_size]
+        for cid, pred in enumerate(all_candidates):
+            recover_pred=Recovery_CoCoNut_one(buggy_line,pred)
+            buggy_method[err_line] = recover_pred
+            patch_method = '\n'.join(buggy_method)
+            patches_id[str(cid + 1)] = patch_method
+        patches_all[id] = patches_id
+    with open(output_f, 'w', encoding='utf8') as f:
+        json.dump(patches_all, f, indent=2)
+
 
